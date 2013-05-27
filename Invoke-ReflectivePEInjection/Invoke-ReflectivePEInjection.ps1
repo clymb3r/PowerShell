081fb53021a7819082ca6c4a7732ae0480c7cfa8
clymb3r <none@none.com>
2013-05-27 10:23:45 -0700

    Cleanup & adding info to the help section

--- Invoke-ReflectivePEInjection/Invoke-ReflectivePEInjection.ps1
+++ Invoke-ReflectivePEInjection/Invoke-ReflectivePEInjection.ps1
@@ -1,11 +1,29 @@
 ﻿<#
 .SYNOPSIS
 
-Reflectively loads a DLL or EXE in to memory of the Powershell process.
+This script has two modes. It can reflectively load a DLL/EXE in to the PowerShell process, 
+or it can reflectively load a DLL in to a remote process. These modes have different parameters and constraints, 
+please lead the Notes section (GENERAL NOTS) for information on how to use them.
+
+
+1.)Reflectively loads a DLL or EXE in to memory of the Powershell process.
 Because the DLL/EXE is loaded reflectively, it is not displayed when tools are used to list the DLLs of a running process.
 
 This tool can be run on remote servers by supplying a local Windows PE file (DLL/EXE) to load in to memory on the remote system,
 this will load and execute the DLL/EXE in to memory without writing any files to disk.
+
+
+2.) Reflectively load a DLL in to memory of a remote process.
+As mentioned above, the DLL being reflectively loaded won't be displayed when tools are used to list DLLs of the running remote process.
+
+This is probably most useful for injecting backdoors in SYSTEM processes in Session0. Currently, you cannot retrieve output
+from the DLL. The script doesn't wait for the DLL to complete execution, and doesn't make any effort to cleanup memory in the 
+remote process. 
+
+
+While this script provides functionality to specify a file to load from disk or from a URL, these are more for demo purposes. The way I'd recommend using the script is to create a byte array
+containing the file you'd like to reflectively load, and hardcode that byte array in to the script. One advantage of doing this is you can encrypt the byte array and decrypt it in memory, which will
+bypass A/V. Another advantage is you won't be making web requests.
 
 PowerSploit Function: Invoke-ReflectivePEInjection
 Author: Joe Bialek, Twitter: @JosephBialek
@@ -15,11 +33,11 @@ Optional Dependencies: None
 
 .DESCRIPTION
 
-Reflectively loads a Windows PE file (DLL/EXE) in to the powershell process.
+Reflectively loads a Windows PE file (DLL/EXE) in to the powershell process, or reflectively injects a DLL in to a remote process.
 
 .PARAMETER PEPath
 
-The path of the DLL/EXE to load and execute.
+The path of the DLL/EXE to load and execute. This file must exist on the computer the script is being run on, not the remote computer.
 
 .PARAMETER PEUrl
 
@@ -27,12 +45,21 @@ A URL containing a DLL/EXE to load and execute.
 
 .PARAMETER ComputerName
 
-Optional, an array of computernames to run the script on
+Optional, an array of computernames to run the script on.
 
 .PARAMETER FuncReturnType
 
 Optional, the return type of the function being called in the DLL. Default: Void
 	Options: String, WString, Void. See notes for more information.
+	IMPORTANT: For DLLs being loaded remotely, only Void is supported.
+	
+.PARAMETER ProcName
+
+Optional, the name of the remote process to inject the DLL in to. If not injecting in to remote process, ignore this.
+
+.PARAMETER ProcId
+
+Optional, the process ID of the remote process to inject the DLL in to. If not injecting in to remote process, ignore this.
 	
 .EXAMPLE
 
@@ -56,7 +83,32 @@ Invoke-ReflectivePEInjection -PEPath DemoDLL.dll -FuncReturnType WString -Comput
 Load DemoEXE and run it locally.
 Invoke-ReflectivePEInjection -PEPath DemoEXE.exe -ExeArgs "Arg1 Arg2 Arg3 Arg4"
 
+.EXAMPLE
+
+Refectively load DemoDLL_RemoteProcess.dll in to the lsass process.
+Invoke-ReflectivePEInjection -PEPath DemoDLL_RemoteProcess.dll -ProcName lsass
+
 .NOTES
+GENERAL NOTES:
+The script has 3 basic sets of functionality:
+1.) Reflectively load a DLL in to the PowerShell process
+	-Can return DLL output to user when run remotely or locally.
+	-Cleans up memory in the PS process once the DLL finishes executing.
+	-Great for running pentest tools on remote computers without triggering process monitoring alerts.
+	-By default, takes 3 function names, see below (DLL LOADING NOTES) for more info.
+2.) Reflectively load an EXE in to the PowerShell process.
+	-Can NOT return EXE output to user when run remotely. If remote output is needed, you must use a DLL. CAN return EXE output if run locally.
+	-Cleans up memory in the PS process once the DLL finishes executing.
+	-Great for running existing pentest tools which are EXE's without triggering process monitoring alerts.
+3.) Reflectively inject a DLL in to a remote process.
+	-Can NOT return DLL output to the user when run remotely OR locally.
+	-Does NOT clean up memory in the remote process if/when DLL finishes execution.
+	-Great for planting backdoor on a system by injecting backdoor DLL in to another processes memory.
+	-Expects the DLL to have this function: void VoidFunc(). This is the function that will be called after the DLL is loaded.
+
+
+
+DLL LOADING NOTES:
 
 PowerShell does not capture an applications output if it is output using stdout, which is how Windows console apps output.
 If you need to get back the output from the PE file you are loading on remote computers, you must compile the PE file as a DLL, and have the DLL
@@ -120,15 +172,15 @@ Param(
 	[String]
 	$FuncReturnType = 'Void',
 	
-	[Parameter(Position = 3, Mandatory = $false)]
+	[Parameter(Position = 3)]
 	[String]
 	$ExeArgs,
 	
-	[Parameter(Position = 4, Mandatory = $false)]
+	[Parameter(Position = 4)]
 	[Int32]
 	$ProcId,
 	
-	[Parameter(Position = 5, Mandatory = $false)]
+	[Parameter(Position = 5)]
 	[String]
 	$ProcName
 )
@@ -1062,6 +1114,7 @@ $RemoteScriptBlock = {
 		
 		return $RemoteThreadHandle
 	}
+
 	
 
 	Function Get-ImageNtHeaders
@@ -2570,9 +2623,11 @@ $RemoteScriptBlock = {
 			{
 				Throw "Can't find process $ProcName"
 			}
-			elseif ($Processes.Count -gt 1)#todo make this error message list processes, pids, x64/x86, session, etc
+			elseif ($Processes.Count -gt 1)
 			{
-				Throw "More than one instance of $ProcName found, please specify the process ID to inject in to"
+				$ProcInfo = Get-Process | where { $_.Name -eq $ProcName } | Select-Object ProcessName, Id, SessionId
+				Write-Output $ProcInfo
+				Throw "More than one instance of $ProcName found, please specify the process ID to inject in to."
 			}
 			else
 			{
