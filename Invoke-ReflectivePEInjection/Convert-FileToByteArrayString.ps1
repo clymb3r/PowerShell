fc39b6e3967f7d7564dac15a93f537463649559d
clymb3r <bialek.joseph@gmail.com>
2013-06-13 23:02:01 -0700

    Adding Invoke-NinjaCopy
    Invoke-NinjaCopy can copy files by parsing NTFS volumes. This allows it
    to bypass file locks, DACL's, and SACL's.

---
+++ Invoke-ReflectivePEInjection/Convert-FileToByteArrayString.ps1
@@ -0,0 +1,19 @@
+﻿Param(
+[Parameter(Position=0, Mandatory=$true)]
+[String]
+$File
+)
+
+[Byte[]]$Bytes = [System.IO.File]::ReadAllBytes($File)
+
+$ByteStr = ""
+for ($i = 0; $i -lt $Bytes.Length; $i++)
+{
+    $ByteStr += $Bytes[$i]
+    if ($i -ne ($Bytes.Length-1))
+    {
+        $ByteStr += ","
+    }
+}
+
+return $ByteStr
